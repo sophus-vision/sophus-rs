0d721a15687929ffd545a483c04e0b2a2fbe4687
Hauke Strasdat <strasdat@gmail.com>
2024-10-27 10:37:52 -0700

    rm pyo3 - for now

--- publish_all.sh
+++ publish_all.sh
@@ -5,7 +5,6 @@ set -e # exit on error
 
 cargo publish -p sophus_core
 cargo publish -p sophus_lie
-cargo publish -p sophus_pyo3
 cargo publish -p sophus_image
 cargo publish -p sophus_sensor
 cargo publish -p sophus_opt
