3d0c62bef188629775a10fffdbb3d853543feb89
Hauke Strasdat <strasdat@gmail.com>
2024-12-07 12:33:34 -0800

    bump to 0.11 (#44)

--- publish_all.sh
+++ publish_all.sh
@@ -8,4 +8,7 @@ cargo publish -p sophus_lie
 cargo publish -p sophus_image
 cargo publish -p sophus_sensor
 cargo publish -p sophus_opt
+cargo publish -p sophus_renderer
+cargo publish -p sophus_sim
+cargo publish -p sophus_viewer
 cargo publish -p sophus
