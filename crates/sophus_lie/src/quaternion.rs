7abbc6acad20a3a793f6635f9a9f4e13dc15b701
Hauke Strasdat <strasdat@gmail.com>
2025-06-02 18:43:03 -0700

    feat: add quaternion manifold

--- crates/sophus_lie/src/quaternion.rs
+++ crates/sophus_lie/src/quaternion.rs
@@ -20,6 +20,7 @@ pub struct Quaternion<
     const DN: usize,
 > {
     params: S::Vector<4>,
+    phantom: PhantomData<S>,
 }
 
 impl<S: IsScalar<BATCH, DM, DN>, const BATCH: usize, const DM: usize, const DN: usize>
@@ -29,6 +30,7 @@ impl<S: IsScalar<BATCH, DM, DN>, const BATCH: usize, const DM: usize, const DN: 
     pub fn from_params(params: S::Vector<4>) -> Self {
         Self {
             params,
+            phantom: PhantomData,
         }
     }
 
